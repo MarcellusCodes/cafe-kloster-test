79edf152897bb70b9ea973e258af8c63e4fcfb58
Pohl <m.pohl@drk-rostock.de>
2023-02-15 16:34:31 +0100

    removed gap from content two

--- components/content-two.tsx
+++ components/content-two.tsx
@@ -6,7 +6,7 @@ const ContentTwo = () => {
   const arr = [1, 2, 3, 4, 5];
   return (
     <Section className="h-full bg-primary-900">
-      <Container className="flex h-full flex-col gap-12">
+      <Container className="flex h-full flex-col gap-12 lg:gap-0">
         <div className="mb-12 flex flex-col items-start gap-6">
           <h2 className="font-text text-xl font-semibold text-tertiary-300">
             Unsere Arbeit
@@ -22,7 +22,7 @@ const ContentTwo = () => {
               index % 2 === 1 ? "lg:flex-row-reverse" : "lg:flex-row"
             }`}
           >
-            <div className="grid h-[400px] lg:mt-0 mt-6 w-full place-items-center rounded-sm border-2 border-gray-300 border-opacity-10 bg-primary-900 bg-opacity-80  bg-[url('/content-2-pattern.svg')] bg-no-repeat md:h-[700px] lg:w-1/2">
+            <div className="mt-6 grid h-[400px] w-full place-items-center rounded-sm border-2 border-gray-300 border-opacity-10 bg-primary-900 bg-opacity-80 bg-[url('/content-2-pattern.svg')]  bg-no-repeat md:h-[700px] lg:mt-0 lg:w-1/2">
               <div className="relative h-[80%] w-[80%]">
                 <Image
                   alt="test"
