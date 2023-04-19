def5e5c8ea8985f68fda7712cbefed7e66aad635
Pohl <m.pohl@drk-rostock.de>
2023-04-19 10:31:30 +0200

    update

--- components/first-loading-screen.tsx
+++ components/first-loading-screen.tsx
@@ -3,7 +3,6 @@ import dynamic from "next/dynamic";
 
 const CoffeeAnimation = dynamic(() => import("./lottie-coffee-animation"), {
   loading: () => <span></span>,
-  ssr: false,
 });
 
 const FirstLoadingScreen = () => {
